0c4ee6689b54e46af6fa474d7dac75fabbcd6ebd
Manish Kumar <strugglelifemanish@gmail.com>
2022-12-01 22:44:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import {useState } from "react";
+import axios from "axios";
+
+function App() {
+  const apiKey = "f56f24967aaf51182d1d4df628297c6d"
+  const [inputCity, setInputCity] = useState("")
+  const [data, setData] = useState({})
+
+
+  const getWeatherDetails = (cityName) => {
+    if (!cityName) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    axios.get(apiURL).then((res) => {
+      console.log("response", res.data)
+      setData(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  }
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value)
+    setInputCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    getWeatherDetails(inputCity)
+  }
+
+
+  return (
+    <div className="col-md-12">
+      <div className="weatherBg">
+        <h1 className="heading">Weather App</h1>
+        <div className="d-grid gap-3 col-4 at-4">
+          <input type="text" className="form-control"  value={inputCity} onChange={handleChangeInput} />
+          <button className="btn btn-info" type="button" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+      </div>
+
+      {Object.keys(data).length > 0 &&
+        <div className="col-md-12 text-center mt-5">
+
+          <div className="shadow rounded weatherResultBox">
+            <img alt="backgroundImage" className="weatherIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
+
+            <h5 className="weatherCity">
+              {data?.name}
+            </h5>
+            <h6 className="weatherTemp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+          </div>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default App;
